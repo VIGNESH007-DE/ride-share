2ef63964f63254278c947bf2a5055597e16d42c7
VIGNESH007-DE <vigneshwaranethicalhacker@gmail.com>
2026-04-19 12:10:06 +0530

    Update firebase.ts

--- modern-ride-sharing-web-application/src/lib/firebase.ts
+++ modern-ride-sharing-web-application/src/lib/firebase.ts
@@ -4,12 +4,13 @@ import { getFirestore } from "firebase/firestore";
 
 // These should be replaced with actual Firebase project configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCdQz4N6MfLHieI5JYBHjdRrKXyYEsf-OM",
+  authDomain: "wayback-a31a2.firebaseapp.com",
+  projectId: "wayback-a31a2",
+  storageBucket: "wayback-a31a2.firebasestorage.app",
+  messagingSenderId: "1071532786218",
+  appId: "1:1071532786218:web:e727812ba851940f553953",
+  measurementId: "G-FG9SD1ZFD1"
 };
 
 const app = initializeApp(firebaseConfig);
